a5f2be382765111bd2bfebe5409af1cc70b4527c
theoneqq <804171087@qq.com>
2017-06-11 00:12:43 +0800

    topcoder ab

--- topcoder/ab.cpp
+++ topcoder/ab.cpp
@@ -1,30 +1,56 @@
 #include <iostream>
-
+#include <sstream>
+#include <vector>
+using namespace std;
 string ab(int n, int k);
-string ab(int a, int b, int k);
-
+bool ab(int a, int b, int k, std::vector<int>& res);
+void print(const std::vector<int>& res);
 
 
 int main() {
+    ab(5, 5);
+    ab(8, 10);
 	return 0;
 }
 
-string ab(int n, int k) {
-	
+void print(const std::vector<int>& res, std::ostringstream& os) {
+   for (size_t i = 0; i < res.size(); ++i) {
+        for (int j = 0; j < res[i]; ++j) {
+            os << "B";
+        }
+        if (i < res.size() - 1) {
+            os << "A";
+        }
+   }
+   os << endl;
+   cout << os.str();
 }
 
-bool ab(int a, int b, int k, std::stack<int>& res) {
-	if (a == 1) {
-		if (b >= k) {
-			res.push(k);
-			res.push(k - b);
-			return true;
-		} else {
-			return false;
-		}
+string ab(int n, int k) {
+    std::vector<int> res;
+    ostringstream os;
+    for (int i = 0; i <= n; ++i) {
+        if (ab(i, n - i, k, res)) {
+            print(res, os);
+            return os.str();
+        } 
+    }    
+    return "";
+}
+
+
+bool ab(int a, int b, int k, std::vector<int>& res) {
+    if (a == 0) {
+        if (k == 0) {
+            res.push_back(b);
+            return true;
+        } else {
+            return false;
+        }
 	}
 	for (int i = 0; i <= b; ++i) {
-		if (ab(a - 1, b - i, k - a * i, res)) res.push(i);
-		if (b + 1 == (int) res.size()) return true;
+		if (ab(a - 1, b - i, k - a * i, res)) res.push_back(i);
+		if (a + 1 == (int) res.size()) return true;
 	}
+    return false;
 }
