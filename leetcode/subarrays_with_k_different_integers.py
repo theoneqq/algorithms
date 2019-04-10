c8c63b395737c516602950d3bb055f5bc3c62658
theoneqq <804171087@qq.com>
2019-04-10 04:33:09 -0700

    leetcod992 fix bug

--- leetcode/subarrays_with_k_different_integers.py
+++ leetcode/subarrays_with_k_different_integers.py
@@ -13,30 +13,24 @@ class solution:
         list_len = len(a)
 
         l = 0
+        m = 0
         r = 0
         while l <= r and r < list_len:
             cur = a[r]
-            if cur not in counts:
-                counts[cur] = 1
-            else:
-                counts[cur] += 1
+            counts[cur] = counts.get(cur, 0) + 1
             counts_len = self.get_len(counts)
             print('counts', counts)
-            if counts_len > k:
-                while l <= r:
-                    cur = a[l]
+            if self.get_len(counts) > k:
+                while m <= r:
+                    cur = a[m]
                     counts[cur] -= 1
-                    if self.get_len(counts) < k:
-                        if cur not in counts:
-                            counts[cur] = 1
-                        else:
-                            counts[cur] += 1
+                    m += 1
+                    if self.get_len(counts) == k:
                         break
-                    l += 1
-                count += 1
+                l = m
                 print('>', l, r)
-            elif counts_len == k:
-                m = l
+            
+            if self.get_len(counts) == k:
                 while m <= r:
                     cur = a[m]
                     if counts[cur] == 1:
@@ -45,16 +39,11 @@ class solution:
                         counts[cur] -= 1
                     m += 1
                 print('<', l, m)
-                for i in range(l, m):
-                    cur = a[i]
-                    if cur not in counts:
-                        counts[cur] = 1
-                    else:
-                        counts[cur] += 1
                 count += m - l + 1
             r += 1
         return count
 
-print('result: {0}'.format(solution().subarray_with_k_distinct([27,27,43,28,11,20,1,4,49,18,37,31,31,7,3,31,50,6,50,46,4,13,31,49,15,52,25,31,35,4,11,50,40,1,49,14,46,16,11,16,39,26,13,4,37,39,46,27,49,39,49,50,37,9,30,45,51,47,18,49,24,24,46,47,18,46,52,47,50,4,39,22,50,40,3,52,24,50,38,30,14,12,1,5,52,44,3,49,45,37,40,35,50,50,23,32,1,2], 20)))
+#print('result: {0}'.format(solution().subarray_with_k_distinct([27,27,43,28,11,20,1,4,49,18,37,31,31,7,3,31,50,6,50,46,4,13,31,49,15,52,25,31,35,4,11,50,40,1,49,14,46,16,11,16,39,26,13,4,37,39,46,27,49,39,49,50,37,9,30,45,51,47,18,49,24,24,46,47,18,46,52,47,50,4,39,22,50,40,3,52,24,50,38,30,14,12,1,5,52,44,3,49,45,37,40,35,50,50,23,32,1,2], 20)))
                 
+print('result: {0}'.format(solution().subarray_with_k_distinct([2,1,1,1,2], 2)))
 
