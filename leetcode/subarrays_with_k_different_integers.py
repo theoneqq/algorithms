8c8ddc7174804d3a136091e2a9ad922f39c15848
theoneqq <804171087@qq.com>
2019-04-11 18:03:26 +0800

    fix leetcode992 bug

--- leetcode/subarrays_with_k_different_integers.py
+++ leetcode/subarrays_with_k_different_integers.py
@@ -1,44 +1,37 @@
-class solution:
-    def get_len(self, a: {}) -> int:
-        count = 0
-        for num in a:
-            if a[num] > 0:
-                count += 1
-        return count
-
-
-    def subarray_with_k_distinct(self, a: [int], k: int) -> int:
+class Solution: 
+    def subarraysWithKDistinct(self, A: List[int], K: int) -> int:
         count = 0
         counts = {}
-        list_len = len(a)
+        list_len = len(A)
 
         l = 0
         m = 0
         r = 0
         while l <= r and r < list_len:
-            cur = a[r]
+            cur = A[r]
             counts[cur] = counts.get(cur, 0) + 1
-            counts_len = self.get_len(counts)
-            print('counts', counts)
-            if self.get_len(counts) > k:
+            counts_len = len(counts)
+            if len(counts) > K:
                 while m <= r:
-                    cur = a[m]
+                    cur = A[m]
                     counts[cur] -= 1
+                    if counts[cur] == 0:
+                        del counts[cur]
                     m += 1
-                    if self.get_len(counts) == k:
+                    if len(counts) == K:
                         break
                 l = m
-                print('>', l, r)
             
-            if self.get_len(counts) == k:
+            if len(counts) == K:
                 while m <= r:
-                    cur = a[m]
+                    cur = A[m]
                     if counts[cur] == 1:
                         break
                     else:
                         counts[cur] -= 1
+                        if counts[cur] == 0:
+                            del counts[cur]
                     m += 1
-                print('<', l, m)
                 count += m - l + 1
             r += 1
         return count
