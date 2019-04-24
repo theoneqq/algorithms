77c5363ce09d118ce55de4b787b1cdec5d3e266c
theoneqq <804171087@qq.com>
2019-04-23 23:09:21 -0700

    leetcode87 simplfiy code

--- leetcode/scramble_string.py
+++ leetcode/scramble_string.py
@@ -3,29 +3,25 @@ class solution:
         self.dp = {}
 
     def is_scramble(self, s1, s2) -> bool:
-        comb = s1 + '#' + s2
-        if comb in self.dp:
-            return self.dp[comb]
+        if (s1, s2) in self.dp:
+            return self.dp[(s1, s2)]
 
         res = False
         s_len = len(s1)
         if s_len < 2:
             res = s1 == s2
-            self.dp[comb] = res
+            self.dp[(s1, s2)] = res
             return res
 
         for i in range(1, s_len):
-            s1_left, s1_right = s1[:i], s1[i:]
-            s2_left1, s2_right1 = s2[:i], s2[i:]
-            if self.is_scramble(s1_left, s2_left1) and self.is_scramble(s1_right, s2_right1):
+            if self.is_scramble(s1[:i], s2[:i]) and self.is_scramble(s1[i:], s2[i:]):
                 res = True
                 break
-            s2_left2, s2_right2 = s2[-i:], s2[:-i]
-            if self.is_scramble(s1_left, s2_left2) and self.is_scramble(s1_right, s2_right2):
+            if self.is_scramble(s1[:i], s2[-i:]) and self.is_scramble(s1[i:], s2[:-i]):
                 res = True
                 break
 
-        self.dp[comb] = res
+        self.dp[(s1, s2)] = res
         return res
 
 print('result: {0}'.format(solution().is_scramble('abcdefghijklmnopq', 'efghijklmnopqcadb')))
