59caaac2302797ca14afcdb364db5645123e2a66
theoneqq <804171087@qq.com>
2019-04-23 22:31:29 -0700

    leetcode87 add dp cache

--- leetcode/scramble_string.py
+++ leetcode/scramble_string.py
@@ -1,18 +1,33 @@
 class solution:
+    def __init__(self):
+        self.dp = {}
+
     def is_scramble(self, s1, s2) -> bool:
+        comb = s1 + s2
+        if comb in self.dp:
+            return self.dp[comb]
+
+        res = False
         s_len = len(s1)
         if s_len < 2:
-            return s1 == s2
+            res = s1 == s2
+            self.dp[comb] = res
+            return res
 
         for i in range(1, s_len):
             s1_left, s1_right = s1[:i], s1[i:]
             s2_left1, s2_right1 = s2[:i], s2[i:]
-            s2_left2, s2_right2 = s2[-i:], s2[:-i]
-            print(s1_left, s1_right, s2_left1, s2_right1, s2_left2, s2_right2)
-            if (self.is_scramble(s1_left, s2_left1) and self.is_scramble(s1_right, s2_right1)) or (self.is_scramble(s1_left, s2_left2) and self.is_scramble(s1_right, s2_right2)):
-                return True
+            if self.is_scramble(s1_left, s2_left1) and self.is_scramble(s1_right, s2_right1):
+                res = True
+                break
+            if i != (s_len >> 1):
+                s2_left2, s2_right2 = s2[-i:], s2[:-i]
+                if self.is_scramble(s1_left, s2_left2) and self.is_scramble(s1_right, s2_right2):
+                    res = True
+                    break
 
-        return False
+        self.dp[comb] = res
+        return res
 
-
-print('result: {0}'.format(solution().is_scramble('abb', 'bba')))
+print('result: {0}'.format(solution().is_scramble('abcdefghijklmnopq', 'efghijklmnopqcadb')))
+print('result: {0}'.format(solution().is_scramble('abcd', 'cadb')))
