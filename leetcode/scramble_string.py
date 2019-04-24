1739b351c110f154b95a95ccf960e2b1072ff94f
theoneqq <804171087@qq.com>
2019-04-23 22:15:34 -0700

    leetcode87 fix bug

--- leetcode/scramble_string.py
+++ leetcode/scramble_string.py
@@ -4,14 +4,15 @@ class solution:
         if s_len < 2:
             return s1 == s2
 
-        for i in range(1, s_len - 1):
-            s1_left, s1_right = s1[0:i], s1[i:-1]
-            s2_left1, s2_right1 = s2[0:i], s2[i:-1]
-            s2_left2, s2_right2 = s2_right1, s2_left1
+        for i in range(1, s_len):
+            s1_left, s1_right = s1[:i], s1[i:]
+            s2_left1, s2_right1 = s2[:i], s2[i:]
+            s2_left2, s2_right2 = s2[-i:], s2[:-i]
+            print(s1_left, s1_right, s2_left1, s2_right1, s2_left2, s2_right2)
             if (self.is_scramble(s1_left, s2_left1) and self.is_scramble(s1_right, s2_right1)) or (self.is_scramble(s1_left, s2_left2) and self.is_scramble(s1_right, s2_right2)):
                 return True
 
         return False
 
 
-print('result: {0}'.format(solution().is_scramble('r', 's')))
+print('result: {0}'.format(solution().is_scramble('abb', 'bba')))
