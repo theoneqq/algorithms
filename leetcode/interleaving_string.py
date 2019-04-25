0897ddf4d0fe5bfd3b8c2f2622e62d6e86057d7e
theoneqq <804171087@qq.com>
2019-04-24 21:46:48 -0700

    leetcode97 fix bug

--- leetcode/interleaving_string.py
+++ leetcode/interleaving_string.py
@@ -5,27 +5,25 @@ class solution:
 
         res = False
         s1, s2, s3 = self.s1, self.s2, self.s3
-        if idx3 >= len(s3):
-            res = True
-        elif idx1 >= len(s1) and idx2 >= len(s2):
-            res = idx3 >= len(s3)
+        if idx1 >= len(s1):
+            res = s2[idx2:] == s3[idx3:]
+        elif idx2 >= len(s2):
+            res = s1[idx1:] == s3[idx3:]
         else:
-            if idx1 < len(s1):
-                res = res or self.helper(idx1 + 1, idx2, idx3)
-                if s1[idx1:idx1 + 1] == s3[idx3:idx3 + 1]:
-                    res = res or self.helper(idx1 + 1, idx2, idx3 + 1)
-            if idx2 < len(s2):
-                res = res or self.helper(idx1, idx2 + 1, idx3)
-                if s2[idx2:idx2 + 1] == s3[idx3:idx3 + 1]:
-                    res = res or self.helper(idx1, idx2 + 1, idx3 + 1)
+            if s1[idx1:idx1 + 1] == s3[idx3:idx3 + 1]:
+                res = res or self.helper(idx1 + 1, idx2, idx3 + 1)
+            if s2[idx2:idx2 + 1] == s3[idx3:idx3 + 1]:
+                res = res or self.helper(idx1, idx2 + 1, idx3 + 1)
 
         self.dp[(idx1, idx2, idx3)] = res
         return res
 
     def is_interleave(self, s1, s2, s3) -> bool:
+        if len(s1) + len(s2) != len(s3):
+            return False
         self.dp = {}
         self.s1, self.s2, self.s3 = s1, s2, s3
 
         return self.helper(0, 0, 0)
 
-print('result: {0}'.format(solution().is_interleave('', '', '')))
+print('result: {0}'.format(solution().is_interleave('a', '', 'a')))
