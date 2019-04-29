d2e4d65ada35763d489eeb33b8a1408bd97c6917
theoneqq <804171087@qq.com>
2019-04-29 01:30:47 -0700

    leetcode99 fix bug

--- leetcode/recover_binary_tree.py
+++ leetcode/recover_binary_tree.py
@@ -1,31 +1,16 @@
 class tree_node:
     def __init__(self, x):
-        self.x = x
+        self.val = x
         self.left = None
         self.right = None
 
 class solution:
     def helper(self, root):
         if root.left != None:
-            root.left.left_parent = root
             self.helper(root.left)
         self.v_nodes.append(root)
         if root.right != None:
-            root.right.right_parent = root
             self.helper(root.right)
-
-    def swap(self, node1, node2):
-        node1.left, node2.left = node2.left, node1.left
-        node1.right, node2.right = node2.right, node1.right
-
-        if hasattr(node1, 'left_parent'):
-            node1.left_parent.left = node2
-        elif hasattr(node1, 'right_parent'):
-            node1.right_parent.right = node2
-        if hasattr(node2, 'left_parent'):
-            node2.left_parent.left = node1
-        elif hasattr(node2, 'right_parent'):
-            node2.right_parent.right = node1
 
     def recover_tree(self, root):
         self.v_nodes = []
@@ -45,13 +30,14 @@ class solution:
                 m_idxs.append(i)
 
         node1, node2 = self.v_nodes[m_idxs[0]], self.v_nodes[m_idxs[1]]
-        self.swap(node1, node2)
+        node1.val, node2.val = node2.val, node1.val
 
 
 n1 = tree_node(1)
 n2 = tree_node(2)
 n3 = tree_node(3)
 
-n1.left = n1
-n2.right = n3
+n1.left = n3
+n3.right = n2
+
 solution().recover_tree(n1)
