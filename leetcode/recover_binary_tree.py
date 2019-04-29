447f8e2070107ba2b8d08fe5a2b880998061092e
theoneqq <804171087@qq.com>
2019-04-29 04:17:30 -0700

    leetcode99 bubble swap

--- leetcode/recover_binary_tree.py
+++ leetcode/recover_binary_tree.py
@@ -16,35 +16,42 @@ class solution:
         self.v_nodes = []
         self.helper(root)
 
-        r_node_vals = [ node.val for node in self.v_nodes]
-        l = len(r_node_vals)
-        for i in range(0, l):
-            j = 0
-            while j + 1 < l and r_node_vals[j] > r_node_vals[j + 1]:
-                r_node_vals[j], r_node_vals[j + 1] = r_node_vals[j + 1], r_node_vals[j]
-                j += 1
+        node_vals = [node.val for node in self.v_nodes]
+        l = len(node_vals)
+
+        m_count = 0
+        for i in range(0, l - 1):
+            if node_vals[i] < node_vals[i + 1]:
+                continue
+            m_count += 1
+            while i + 1 < l and node_vals[i] > node_vals[i + 1]:
+                node_vals[i], node_vals[i + 1] = node_vals[i + 1], node_vals[i]
+                i += 1
+            if m_count >= 2:
+                break
 
         for node in self.v_nodes:
             print(node.val)
-        for val in r_node_vals:
+        for val in node_vals:
             print(val)
         
         m_idxs = []
         for i in range(0, l):
-            if self.v_nodes[i].val != r_node_vals[i]:
+            if self.v_nodes[i].val != node_vals[i]:
                 m_idxs.append(i)
 
         node1, node2 = self.v_nodes[m_idxs[0]], self.v_nodes[m_idxs[1]]
         node1.val, node2.val = node2.val, node1.val
 
 
-n1 = tree_node(1)
-n2 = tree_node(2)
-n3 = tree_node(3)
+n1 = tree_node(3)
+n2 = tree_node(1)
+n3 = tree_node(4)
+n4 = tree_node(2)
 
-n1.left = n3
-n3.right = n2
+n1.left = n2
+n1.right = n3
+n3.left = n4
 
 solution().recover_tree(n1)
 
-print(n1.val, n2.val, n3.val)
