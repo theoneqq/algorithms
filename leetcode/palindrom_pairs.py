59ae012e4e4957e360f2dbe6ed06113ef89d1a37
theoneqq <804171087@qq.com>
2019-05-05 01:25:40 -0700

    leetcode336 fix bug

--- leetcode/palindrom_pairs.py
+++ leetcode/palindrom_pairs.py
@@ -1,5 +1,7 @@
 class solution:
     def check_imp(self, idx, p_word, direction):
+        print('cur rhash:', self.r_hash)
+        print('check:', idx, self.words[idx], p_word, direction)
         if p_word in self.w_hash:
             p_idx = self.w_hash[p_word]
             if p_idx != idx:
@@ -11,31 +13,32 @@ class solution:
                         self.r_hash[(p_idx, idx)] = 1
                     else:
                         self.r_hash[(idx, p_idx)] = 1
+        print('now rhash:', self.r_hash)
 
     def check_left(self, idx):
         word = self.words[idx]
         wl = len(word)
-        start, end, step = 0, wl, 1
-        while start < end:
+        start, end, step = wl - 1, 0, -1
+        while start > end:
             l1, l2, r = word[start+1:], word[start:], word[:start+1][::-1]
             if r.find(l1) == 0:
                 self.check_imp(idx, r[len(l1):], 1)
             if r.find(l2) == 0:
                 self.check_imp(idx, r[len(l2):], 1)
-            start += 1
+            start -= 1
 
     def check_right(self, idx):
         word = self.words[idx]
         wl = len(word)
-        start, end, step = wl - 1, 0, -1
-        while start > end:
+        start, end, step = 0, wl, 1
+        while start < end:
             l1, l2, r = word[:start], word[:start+1], word[start:]
             rl1, rl2 = l1[::-1], l2[::-1]
             if r.find(rl1) == 0:
                 self.check_imp(idx, r[len(rl1):][::-1], 0)
             if r.find(rl2) == 0:
                 self.check_imp(idx, r[len(rl2):][::-1], 0)
-            start -= 1
+            start += 1
 
 
     def check(self, idx):
@@ -58,6 +61,6 @@ class solution:
 
         return [ [k[0], k[1]] for k in self.r_hash ]
 
-res = solution().palindrom_pairs(['abcd', 'dcba', 'lls', 's', 'sssll'])
+res = solution().palindrom_pairs(['a', 'b', 'c', 'ab', 'ac', 'aa'])
 for pair in res:
     print(pair[0], pair[1])
