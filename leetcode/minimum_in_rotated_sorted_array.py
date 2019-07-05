6f22604a983a5e1858b4e749bfd1bab8f57370cd
theoneqq <804171087@qq.com>
2019-07-04 22:12:09 -0700

    minimum_in_rotated_sorted_array

--- leetcode/minimum_in_rotated_sorted_array.py
+++ leetcode/minimum_in_rotated_sorted_array.py
@@ -8,7 +8,7 @@ class solution:
                 l_ok = nums[l] <= nums[m]
                 r_ok = nums[m + 1] <= nums[r]
                 if l_ok and r_ok:
-                    return min(nums[l], nums[m + 1])
+                    return min(helper(l, m), helper(m + 1, r))
                 elif not l_ok:
                     return helper(l, m)
                 else:
@@ -17,7 +17,6 @@ class solution:
             return nums[0]
         else:
             return helper(0, len(nums) - 1)
-
 print('result: {0}'.format(solution().find_min([1,3,5])))
 print('result: {0}'.format(solution().find_min([4,5,6,7,0,1,2])))
 print('result: {0}'.format(solution().find_min([2,2,2,0,1])))
@@ -26,3 +25,4 @@ print('result: {0}'.format(solution().find_min([1,1])))
 print('result: {0}'.format(solution().find_min([1,3])))
 print('result: {0}'.format(solution().find_min([3,3,3])))
 print('result: {0}'.format(solution().find_min([3,3,2])))
+print('result: {0}'.format(solution().find_min([10,1,10,10,10])))
