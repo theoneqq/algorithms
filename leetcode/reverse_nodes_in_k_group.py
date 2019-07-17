6d85fae62f46ae92ac04c553f049c4b65b6d8902
theoneqq <804171087@qq.com>
2019-07-17 00:47:02 -0700

    leetcode25 ugly code

--- leetcode/reverse_nodes_in_k_group.py
+++ leetcode/reverse_nodes_in_k_group.py
@@ -10,38 +10,46 @@ class ListNode:
 
 class solution:
     def reverse_nodes(self, head, k):
-        if head == None:
-            return None
-        res, prev, num = head, None, k
-        cur_node, next_node = head, head.next
+        if head == None or k <= 1:
+            return head
 
-        while num > 1 and next_node != None:
-            if num == k and prev != None:
-                prev = cur_node
-            tmp = next_node.next
-            next_node.next = cur_node
-            cur_node, next_node = next_node, tmp
-            num -= 1
-            if num == 1:
-                if prev != None:
-                    prev.next = cur_node
-                else:
-                    res = cur_node
-                num = k
-        print('yoho')
+        res, first, prev, cur, nxt, full = None, None, None, head, None, True
+        def reverse(num):
+            nonlocal res, first, cur, nxt, full
+            if cur == None:
+                return
+            nxt = cur.next
+            cur.next = None
+            while num > 1 and nxt != None:
+                if num == k:
+                    first = cur
+                tmp = nxt.next
+                nxt.next = cur
+                cur, nxt = nxt, tmp
+                num -= 1
+            full = num == 1
+            if res == None and cur != None:
+                res = cur
 
-        if num != 1:
-            next_node = cur_node.next
-            while next_node != None:
-                tmp = next_node.next
-                next_node.next = cur_node
-                cur_node, next_node = next_node, tmp
-            prev.next = cur_node
+        while cur != None and full:
+            reverse(k)
+            if not full:
+                reverse(k)
+            if prev != None:
+                prev.next = cur
+            cur, prev = nxt, first
 
         return res
 
 
-n1, n2, n3 = ListNode(1), ListNode(4), ListNode(5)
+n1, n2, n3, n4, n5 = ListNode(1), ListNode(2), ListNode(3), ListNode(4), ListNode(5)
 n1.next = n2
 n2.next = n3
+n3.next = n4
+n4.next = n5
+#print('result: {0}'.format(solution().reverse_nodes(n1, 0)))
+#print('result: {0}'.format(solution().reverse_nodes(n1, 5)))
+#print('result: {0}'.format(solution().reverse_nodes(n1, 4)))
+print('result: {0}'.format(solution().reverse_nodes(n1, 3)))
 print('result: {0}'.format(solution().reverse_nodes(n1, 2)))
+#print('result: {0}'.format(solution().reverse_nodes(n1, 1)))
